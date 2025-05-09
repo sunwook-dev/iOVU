7a754ec9a63af2bc4cdedbcd639f32ec1267457c
minyy <ymy0613@gmail.com>
2025-05-09 10:09:49 +0900

    주석 삭제

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CommonTitle from "../component/common/CommonTitle";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import CommonButton from "../component/common/CommonButton";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +11,23 @@ const Main = () => {
   };
   return (
     <>
-      <Container sx={{ width: "1000px", display: "flex" }}>
+      <Container
+        sx={{
+          width: "1000px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
         <CommonTitle>Main Page</CommonTitle>
-        <CommonButton onClick={clickBtn}> ssss</CommonButton>
+        <Box sx={{ display: "flex", flexDirection: "column", p: 2 }}>
+          <p>버튼사용 예시</p>
+          <CommonButton onClick={clickBtn}> primary</CommonButton>
+          <CommonButton onClick={clickBtn} variant="outlined">
+            primary
+          </CommonButton>
+          <CommonButton color="fourth">fourth</CommonButton>
+        </Box>
       </Container>
     </>
   );

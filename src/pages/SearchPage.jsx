f49789ea116b550228322cd388125f09b992926a
SunWook Kim <tjsdnr9@gmail.com>
2025-05-12 02:12:32 +0900

    250512 UI 1차수정(랜딩페이지,검색페이지)

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import CommonTitle from "../component/common/CommonTitle";
-import {TextField, InputAdornment, Container, Box } from "@mui/material";
+import {Box, Container, InputAdornment, TextField} from "@mui/material";
+
 import CommonButton from "../component/common/CommonButton";
+import CommonTitle from "../component/common/CommonTitle";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchPage = () => {
@@ -11,13 +12,15 @@ const SearchPage = () => {
       console.log("검색 실행");
     };
   return (
-    <Container sx={{
-      width: "1000px",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-    }}> 
+    <Container
+      sx={{
+        width: "1000px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <Box
         sx={{
           width: "100%",
@@ -25,38 +28,46 @@ const SearchPage = () => {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          paddingTop: "10vh",
-          gap: 2,
+          paddingTop: "1vh",
+          position: "relative",
+          // gap: 2,
         }}
-      >      
-        <img        
-          src="/image/searchBanner.png"
+      >
+        <img
+          src="/image/searchBanner2.png"
           alt="검색 배너"
-          style={{                        
-            width: 500, 
-            height: 300,
-            marginBottom: 20,
-          }}        
+          style={{
+            width: 500,
+            height: 400,
+            marginBottom: 0,
+            marginTop: 0,
+          }}
         />
-      
-       <TextField
+
+        <TextField
           placeholder="키워드 검색"
           variant="outlined"
-          size="medium"          
-          sx={{ width: 500}}
-        InputProps={{
-          sx: {
-              '& input': {
-                paddingLeft: 25, 
-                textAlign: 'left', 
+          size="medium"
+          sx={{ width: 600, position: "absolute", top: "350px" }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+          InputProps={{
+            sx: {
+              "& input": {
+                paddingLeft: 33,
+                textAlign: "left",
               },
               height: 40,
               borderRadius: 5,
               backgroundColor: "#F5F5F5",
             },
             endAdornment: (
-              <InputAdornment position="end" >
-                <CommonButton onClick={handleSearch}
+              <InputAdornment position="end">
+                <CommonButton
+                  onClick={handleSearch}
                   sx={{
                     backgroundColor: "transparent",
                     boxShadow: "none",
@@ -64,27 +75,30 @@ const SearchPage = () => {
                       backgroundColor: "transparent",
                     },
                     "&:hover img": {
-                      transform: "scale(1.2)",    // 확대 효과
-                      filter: "brightness(1.3)",  // 더 밝게
+                      transform: "scale(1.2)", // 확대 효과
+                      filter: "brightness(1.3)", // 더 밝게
                     },
                   }}
                 >
                   <img
-                    src="/image/search.png" 
+                    src="/image/search.png"
                     alt="검색"
                     style={{
                       width: 20,
                       height: 20,
                       transition: "all 0.3s ease",
-                    }}                    
+                    }}
                   />
                 </CommonButton>
               </InputAdornment>
             ),
           }}
-        /> 
-      </Box>    
+        />
+      </Box>
     </Container>
   );
 };
 export default SearchPage;
+
+
+

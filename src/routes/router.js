78b8139b69d04d64abe76cf223b7d01af3404ab9
minyy <ymy0613@gmail.com>
2025-05-13 17:44:09 +0900

    mainpage delete

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,7 @@
 // src/routes/router.js
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Main from "../pages/Main";
 import Login from "../pages/Login";
 import ReportLinkDomainPage from "../pages/ReportLinkDomainPage"; // 기존 페이지
-import ChartTestPage from "../pages/ChartTestPage"; // 새로 만든 테스트 페이지 import
 import LandingPage from "../pages/LandingPage";
 import SearchPage from "../pages/SearchPage";
 import Report from "../pages/Report";

8cac6bc7fde9e7f7bb8968fee2f7e6a02b37b6ed
sunwook <tjsdnr9@gmail.com>
2025-05-15 11:47:16 +0900

    landing페이지 주석제거

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,65 +10,13 @@ const LandingPage = () => {
     navigate("/login");
   };
 
-  // return (
-  //   <Box
-  //     sx={{
-  //       width: "100%",
-  //       height: "68vh",
-  //       backgroundImage: "url('/image/landing.png')",
-  //       backgroundSize: "cover",
-  //       backgroundRepeat: "no-repeat",
-  //       backgroundPosition: "center",
-  //       display: "flex",
-  //       justifyContent: "center",
-  //       alignItems: "center",
-  //     }}
-  //   >
-  //     <Container
-  //       sx={{
-  //         width: "1000px",          
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         alignItems: "center",
-  //         padding: 0, // 불필요한 여백 제거
-  //         margin: 0,
-  //       }}
-  //     >
-  //       <motion.div
-  //         initial={{ opacity: 0, y: -30 }}
-  //         animate={{ opacity: 1, y: 0 }}
-  //         transition={{ duration: 0.8 }}
-  //       >
-  //         <CommonTitle
-  //           sx={{
-  //             fontSize: 36,
-  //             pt: "5vh",
-  //           }}
-  //         >
-  //           AI로 검색을 혁신하세요
-  //         </CommonTitle>
-  //         <CommonTitle sx={{ fontSize: 30 }}>
-  //           키워드 분석부터 트렌드까지, 당신의 마케팅을 더 똑똑하게
-  //         </CommonTitle>
-  //         <CommonButton
-  //           onClick={clickBtn}
-  //           sx={{
-  //             mt: 5,
-  //           }}
-  //         >
-  //           Go to Login
-  //         </CommonButton>
-  //       </motion.div>
-  //     </Container>
-  //   </Box>
-  // );
     return (
     <Container
     maxWidth={false}
     disableGutters
     sx={{
       width: "100%",
-      height: "68vh",
+      height: "70vh",
       minWidth: 0,
       minHeight: 0,
       padding: 0,
@@ -82,7 +30,7 @@ const LandingPage = () => {
       <Box
         sx={{
           width: "100%",
-          height: "68vh",
+          height: "70vh",
           backgroundImage: "url('/image/landing.png')",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
@@ -99,7 +47,7 @@ const LandingPage = () => {
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          style={{ marginTop: '-40px' }}
+          style={{ marginTop: '-35px' }}
         >
           <CommonTitle sx={{ fontSize: 36, textAlign: "center"}}>
             AI로 검색을 혁신하세요
